04d0ee2be8364c89b4624ad68c475ece1b865272
med-aillon <mehdimerghichi@gmail.com>
2023-10-02 20:38:16 +0100

    Reset et animation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let workTime = 5;
-let restTime = 3;
+let workTime = 1800;
+let restTime = 300;
 
 function formatTime(time) {
   return `${Math.floor(time / 60)}:${
@@ -24,13 +24,11 @@ let timerId;
 function togglePomodoro() {
   handlePlayPause();
   if (currentInterval) return;
+  currentInterval = true;
 
   workTime--;
-  currentInterval = true;
   displayWork.textContent = formatTime(workTime);
-  timerId = setInterval(() => {
-    handleTick();
-  }, 1000);
+  timerId = setInterval(handleTick, 1000);
 }
 
 function handlePlayPause() {
@@ -38,10 +36,16 @@ function handlePlayPause() {
     pause = false;
     togglePlayBtn.firstElementChild.src = "/ressources/pause.svg";
     togglePlayBtn.setAttribute("data-toggle", "pause");
+    if (workTime) {
+      handleGetAnimation({ work: true, rest: false });
+    } else {
+      handleGetAnimation({ work: false, rest: true });
+    }
   } else {
     pause = true;
     togglePlayBtn.firstElementChild.src = "/ressources/play.svg";
     togglePlayBtn.setAttribute("data-toggle", "play");
+    handleGetAnimation({ work: false, rest: false });
   }
 }
 
@@ -52,9 +56,11 @@ function handleTick() {
   if (!pause && workTime > 0) {
     workTime--;
     displayWork.textContent = formatTime(workTime);
+    handleGetAnimation({ work: true, rest: false });
   } else if (!pause && !workTime && restTime > 0) {
     restTime--;
     displayRest.textContent = formatTime(restTime);
+    handleGetAnimation({ work: false, rest: true });
   } else if (!pause && !workTime && !restTime) {
     workTime = 4;
     restTime = 3;
@@ -62,5 +68,39 @@ function handleTick() {
     displayRest.textContent = formatTime(restTime);
     cycleNumber++;
     cycles.textContent = `Cycle(s) : ${cycleNumber}`;
+    handleGetAnimation({ work: true, rest: false });
+  }
+}
+
+const resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  workTime = 1799;
+  restTime = 300;
+
+  displayWork.textContent = formatTime(workTime);
+  displayRest.textContent = formatTime(restTime);
+
+  pause = false;
+  currentInterval = false;
+
+  togglePlayBtn.firstElementChild.src = "/ressources/play.svg";
+  togglePlayBtn.setAttribute("data-toggle", "play");
+
+  cycleNumber = 0;
+  cycles.textContent = `Cycle(s) : ${cycleNumber}`;
+
+  clearInterval(timerId);
+  handleGetAnimation({ work: false, rest: false });
+}
+
+function handleGetAnimation(itemState) {
+  for (const item in itemState) {
+    if (itemState[item]) {
+      document.querySelector(`.${item}`).classList.add("active");
+    } else {
+      document.querySelector(`.${item}`).classList.remove("active");
+    }
   }
 }
