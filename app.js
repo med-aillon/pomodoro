c37d001b705e19d4c3d9a0bddc7459e35de5c6ed
med-aillon <mehdimerghichi@gmail.com>
2023-10-02 13:06:55 +0100

    Gérer le décompte et ajout de'pause = true' dans handlePlayPause

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let workTime = 1800;
-let restTime = 300;
+let workTime = 5;
+let restTime = 3;
 
 function formatTime(time) {
   return `${Math.floor(time / 60)}:${
@@ -16,9 +16,21 @@ displayRest.textContent = formatTime(restTime);
 const togglePlayBtn = document.querySelector(".toggle-btn");
 togglePlayBtn.addEventListener("click", togglePomodoro);
 
+let currentInterval = false;
+
 let pause = true;
+let timerId;
+
 function togglePomodoro() {
   handlePlayPause();
+  if (currentInterval) return;
+
+  workTime--;
+  currentInterval = true;
+  displayWork.textContent = formatTime(workTime);
+  timerId = setInterval(() => {
+    handleTick();
+  }, 1000);
 }
 
 function handlePlayPause() {
@@ -27,7 +39,28 @@ function handlePlayPause() {
     togglePlayBtn.firstElementChild.src = "/ressources/pause.svg";
     togglePlayBtn.setAttribute("data-toggle", "pause");
   } else {
+    pause = true;
     togglePlayBtn.firstElementChild.src = "/ressources/play.svg";
     togglePlayBtn.setAttribute("data-toggle", "play");
+  }
+}
+
+const cycles = document.querySelector(".cycle");
+let cycleNumber = 0;
+
+function handleTick() {
+  if (!pause && workTime > 0) {
+    workTime--;
+    displayWork.textContent = formatTime(workTime);
+  } else if (!pause && !workTime && restTime > 0) {
+    restTime--;
+    displayRest.textContent = formatTime(restTime);
+  } else if (!pause && !workTime && !restTime) {
+    workTime = 4;
+    restTime = 3;
+    displayWork.textContent = formatTime(workTime);
+    displayRest.textContent = formatTime(restTime);
+    cycleNumber++;
+    cycles.textContent = `Cycle(s) : ${cycleNumber}`;
   }
 }
